0e3ac51f3bd8b2df5617e05266e4bb5f6b684730
Sam Hurd <sam.p.hurd@vanderbilt.edu>
2016-11-03 11:03:21 -0500

    added redux and simple API call example

--- gatway/index.js
+++ gatway/index.js
@@ -2,6 +2,7 @@
  * Created by Sam on 7/17/2016.
  */
 import Sample from './src/main/webapp/react/components/sample'
+import MainPageProvider from './src/main/webapp/react/components/MainPageProvider'
 //import CompanyPageProvider from './resoft/static/resoft/components/company/CompanyPageProvider'
 //import UnitPageProvider from './resoft/static/resoft/components/unit/UnitPageProvider'
 
--- gatway/src/main/webapp/react/components/sample.js
+++ gatway/src/main/webapp/react/components/sample.js
@@ -1,20 +1,37 @@
 var Sample = React.createClass ({
+
     render() {
-        return (
-          <div className="shopping-list">
-            <h1>Shopping List for {this.props.name}</h1>
-            <ul>
-              <li>Instagram</li>
-              <li>WhatsApp</li>
-              <li>Oculus</li>
-            </ul>
-          </div>
-        );
+    	if(this.props.commitData == undefined){
+            return(<div>Loading</div>)
+        } else {
+            var commit_list = [];
+	        for(var ii = 0; ii<this.props.commitData.length; ii++){
+	            commit_list.push(<li key={ii}>
+	                {this.props.commitData[ii].commit.message}
+	            </li>)
+	        }
+	        return (
+	        <div className="col-md-6">
+				<div className="panel panel-default">
+				  <div className="panel-heading">
+				    <h3 className="panel-title">{this.props.name+" Commits"}</h3>
+				  </div>
+				  <div className="panel-body">
+					  <ul>
+					  	{commit_list}
+					  </ul>
+				  </div>
+				</div>
+			</div>
+	        );
+	    }
             }
 });
 
+export default Sample
+/*
 var root = document.getElementById('sample');
 if(root != null && root != undefined){
 	console.log("found");
     ReactDOM.render(<Sample name="Sam"/>,root);
-}
+}*/
